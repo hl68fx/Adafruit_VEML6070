320787557ca3c678b9c227535ef7b49350169011
Thomas <hl68fx@users.noreply.github.com>
2016-08-10 01:57:05 +0200

    changes for Particle

--- examples/vemltest/vemltest.ino
+++ examples/vemltest/vemltest.ino
@@ -1,4 +1,4 @@
-#include <Wire.h>
+// This #include statement was automatically added by the Particle IDE.
 #include "Adafruit_VEML6070.h"
 
 Adafruit_VEML6070 uv = Adafruit_VEML6070();
@@ -8,7 +8,6 @@ void setup() {
   Serial.println("VEML6070 Test");
   uv.begin(VEML6070_1_T);  // pass in the integration time constant
 }
-
 
 void loop() {
   Serial.print("UV light level: "); Serial.println(uv.readUV());
