3dd13b71993284b61b8210daeca8b5baf8359dd1
Thomas <hl68fx@users.noreply.github.com>
2016-08-10 01:55:36 +0200

    changes für Particle

--- Adafruit_VEML6070.cpp
+++ Adafruit_VEML6070.cpp
@@ -1,26 +1,16 @@
 /*************************************************** 
   This is a library for the Adafruit VEML6070 UV Sensor Breakout
-
   Designed specifically to work with the VEML6070 sensor from Adafruit
   ----> https://www.adafruit.com/products/2899
-
   These sensors use I2C to communicate, 2 pins are required to  
   interface
   Adafruit invests time and resources providing this open source code, 
   please support Adafruit and open-source hardware by purchasing 
   products from Adafruit!
-
   Written by Limor Fried/Ladyada for Adafruit Industries.  
   BSD license, all text above must be included in any redistribution
  ****************************************************/
-
-#if (ARDUINO >= 100)
- #include "Arduino.h"
-#else
- #include "WProgram.h"
-#endif
-#include "Wire.h"
-
+#include "application.h"
 #include "Adafruit_VEML6070.h"
 
 
@@ -41,4 +31,3 @@ uint16_t Adafruit_VEML6070::readUV() {
 
   return uvi;  
 }
-
